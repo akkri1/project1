eff2ed00b57dfb90ab9ea83b631302c8f3a0b2da
akkri1 <108869149+akkri1@users.noreply.github.com>
2022-07-18 04:05:32 -0400

    Create geo.py

---
+++ geo.py
@@ -0,0 +1,11 @@
+# importing modules
+from geopy.geocoders import Nominatim
+ 
+# calling the nominatim tool
+geoLoc = Nominatim(user_agent="GetLoc")
+ 
+# passing the coordinates
+locname = geoLoc.reverse("26.7674446, 81.109758")
+ 
+# printing the address/location name
+print(locname.address)
